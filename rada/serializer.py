ba709f9983303997d8abf073224d7f2b8d8b9ac9
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2017-08-22 21:48:32 +0200

    Rada: Send device url

--- rada/serializer.py
+++ rada/serializer.py
@@ -4,6 +4,11 @@ from rada.models import Dispositivo
 
 
 class DispositivoSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+
+    def get_url(self, obj):
+        return "api/dispositivo/" + obj.nombre + "/%i" % obj.puerto
+
     class Meta:
         model = Dispositivo
         fields = '__all__'
